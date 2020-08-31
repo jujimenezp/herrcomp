30c7091df6e0137e2b883df83f4708a435df785d
jujimenezp <jujimenezp@unal.edu.co>
2020-08-31 13:08:56 -0500

    Ejemplo para usar push y actualizar gitignore para ignorar archivos exe

---
+++ ejemplo_push.cpp
@@ -0,0 +1,11 @@
+//ejemplo para usar push en herrcomp
+#include <iostream>
+
+using namespace std;
+int main(void){
+  char a[150];
+  cout << "Su nombre: ";
+  cin >> a;
+  cout << endl << "Buenos dias " << a << endl;
+  return 0;
+}
