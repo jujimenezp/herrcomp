829f28051159316a19231ec0785cf8cbdbbb7e03
jujimenezp <jujimenezp@unal.edu.co>
2020-09-02 12:17:36 -0500

    analizar error underflow y overflow

---
+++ error_over_under.cpp
@@ -0,0 +1,22 @@
+//analizar errores overflow y underflow
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv){
+  //argc cuenta el numero de argumentos en la linea de comandos
+  //argv guarda los valores de esos argumentos
+  std::cout.precision(15);
+  std::cout.setf(std::ios::scientific);
+  double under=1.0, over=1.0;
+  int N=std::atoi(argv[1]);
+  for(int ii=0; ii<N; ii++){
+    under /=2.0;
+    over *=2.0;
+    std::cout << ii
+	      << "\t"<<under
+	      <<"\t"<<over
+	      <<"\n";
+  }
+  return 0;
+}
