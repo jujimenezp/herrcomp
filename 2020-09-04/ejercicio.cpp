fbcdbdf70746f5d48ad4f4f53518b656a6915c13
jujimenezp <jujimenezp@unal.edu.co>
2020-09-05 12:45:10 -0500

    implementacion de N entero en ejercicio para mejorar la precision de x

--- 2020-09-04/ejercicio.cpp
+++ 2020-09-04/ejercicio.cpp
@@ -5,21 +5,38 @@
 #include <cmath> //se incluye para usar sqrt
 
 typedef float REAL;
+REAL funcion(REAL x); //formula original
+REAL funcionmod(REAL x); //formula moodificada con racionalizacion
+
 int main(void){
-    REAL x=0.0, f, fmod;
+  REAL x=0.0, f, fmod;
+  int N=0;
     //se define los digitos de precision de acuerdo a la variable float
-    std::cout.precision(6); std::cout.setf(std::ios::scientific);
+    std::cout.precision(8); std::cout.setf(std::ios::scientific);
     
     //como x va de 0 a 1 en saltos de 0.01 se hacen 101 iteraciones
     for(int n=0; n<=100; n++){
-        f = 5.0-sqrt(25.0+x*x);             //formula original
-        fmod = -(x*x)/(5.0+sqrt(25.0+x*x)); //formula modificada con racionalizacion
+      f = funcion(x);
+      fmod = funcionmod(x);
         
         std::cout << x
                   << "\t" << f
                   << "\t" << fmod
                   << "\n";
-        x += 0.01;
+        N += 1;          //entero auxiliar para mejorar la precision de x
+        x =(N*1.0)/100;  //x es mas exacto con esta formula que con +=0.01;
     }
     return 0;
+}
+
+REAL funcion(REAL x){
+  REAL f;
+  f=5.0-sqrt(25.0+x*x);
+  return f;
+}
+
+REAL funcionmod(REAL x){
+  REAL fmod;
+  fmod=-(x*x)/(5.0+sqrt(25.0+x*x));
+  return fmod;
 }
