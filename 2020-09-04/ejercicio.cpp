6ca3415e014e8bb15556341f5301fcb5a38f63d6
jujimenezp <jujimenezp@unal.edu.co>
2020-09-05 12:10:09 -0500

    datos del ejercicio 2020-09-04

--- 2020-09-04/ejercicio.cpp
+++ 2020-09-04/ejercicio.cpp
@@ -9,15 +9,17 @@ int main(void){
     REAL x=0.0, f, fmod;
     //se define los digitos de precision de acuerdo a la variable float
     std::cout.precision(6); std::cout.setf(std::ios::scientific);
+    
     //como x va de 0 a 1 en saltos de 0.01 se hacen 101 iteraciones
     for(int n=0; n<=100; n++){
         f = 5.0-sqrt(25+x*x);             //formula original
         fmod = -(x*x)/(5.0+sqrt(25+x*x)); //formula modificada con racionalizacion
+        
         std::cout << x
                   << "\t" << f
                   << "\t" << fmod
                   << "\n";
-        x += 0.01; //salto de 0.01
+        x += 0.01;
     }
     
     return 0;
